c4e0bdc7c8f9fa23cb446f8b1a5ed4c00657f47e
J XD <xdjiaxd@gmail.com>
2023-04-30 13:11:36 +0800

    add vercel

--- shell.nix
+++ shell.nix
@@ -3,7 +3,8 @@ with import <nixpkgs> { };
 pkgs.mkShell {
   nativeBuildInputs = [
     nodejs-19_x
-    nodePackages.firebase-tools
+    # nodePackages.firebase-tools
+    nodePackages.vercel
     yarn
   ];
 }
